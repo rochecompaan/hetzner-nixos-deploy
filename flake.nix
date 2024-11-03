f266bfd23b1bdf143a447aaff0fe7016375a61d4
Roché Compaan <roche@upfrontsoftware.co.za>
2024-11-03 10:39:08 +0200

    feat: create venv

--- flake.nix
+++ flake.nix
@@ -167,12 +167,27 @@
       };
 
       devShell.${system} = pkgs.mkShell {
-        buildInputs = [
-          pkgs.netcat
-          pkgs.sops
-          pkgs.yq
-          pkgs.jq
+        buildInputs = with pkgs; [
+          netcat
+          sops
+          yq
+          jq
+          stdenv.cc.cc.lib
         ];
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+
+          VENV_DIR="$(pwd)/.venv"
+          if [ -d "$VENV_DIR" ]; then
+            echo "Skipping venv creation, '$VENV_DIR' already exists"
+          else
+            echo "Creating new venv environment in path: '$VENV_DIR'"
+            python -m venv "$VENV_DIR"
+          fi
+
+          echo "Activating venv environment"
+          source "$VENV_DIR/bin/activate"
+          '';
       };
     };
 }
