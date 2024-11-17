15dcce49a9a3bcb3491eea2263a7f0451fd3ecbd
Roché Compaan <roche@upfrontsoftware.co.za>
2024-11-17 07:09:56 +0200

    refactor: Remove manual venv creation and activation from flake.nix

--- flake.nix
+++ flake.nix
@@ -166,18 +166,6 @@
           jq
           stdenv.cc.cc.lib
         ];
-        shellHook = ''
-          VENV_DIR="$(pwd)/.venv"
-          if [ -d "$VENV_DIR" ]; then
-            echo "Skipping venv creation, '$VENV_DIR' already exists"
-          else
-            echo "Creating new venv environment in path: '$VENV_DIR'"
-            python -m venv "$VENV_DIR"
-          fi
-
-          echo "Activating venv environment"
-          source "$VENV_DIR/bin/activate"
-        '';
       };
     };
 }
